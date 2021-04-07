09175bd1e036a1c2be8913f642620fbe1bf4feaf
felix-lipski <63930662+felix-lipski@users.noreply.github.com>
2021-04-07 15:01:51 +0200

    Update efi.sh

--- efi.sh
+++ efi.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
-echo "partitioning..."
+read -p "How many swap gigabytes do you want, sir?: " swap_size
+echo "Partitioning..."
 parted /dev/sda -- mklabel gpt
-parted /dev/sda -- mkpart primary 512MiB -2GiB
-parted /dev/sda -- mkpart primary linux-swap -2GiB 100%
+parted /dev/sda -- mkpart primary 512MiB "-${swap_size}GiB"
+parted /dev/sda -- mkpart primary linux-swap "-${swap_size}GiB" 100%
 parted /dev/sda -- mkpart ESP fat32 1MiB 512MiB
 parted /dev/sda -- set 3 esp on
-echo "partitioned successfully"
 
-echo "formatting..."
+echo "Formatting..."
 mkfs.ext4 -L nixos /dev/sda1
 mkswap -L swap /dev/sda2
 mkfs.fat -F 32 -n boot /dev/sda3
-echo "formatted successfully"
 
-echo "mounting..."
+echo "Mounting..."
 mount /dev/disk/by-label/nixos /mnt
 mkdir -p /mnt/boot
 mount /dev/disk/by-label/boot /mnt/boot
 swapon /dev/sda2
-echo "mounted successfully"
