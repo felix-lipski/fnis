9b256c9784b4d1f62c8b49ad317f354950d68038
felix-lipski <63930662+felix-lipski@users.noreply.github.com>
2021-04-07 14:48:00 +0200

    Update efi.sh

--- efi.sh
+++ efi.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
+echo "partitioning..."
 parted /dev/sda -- mklabel gpt
-echo "partitioned"
+parted /dev/sda -- mkpart primary 512MiB -2GiB
+parted /dev/sda -- mkpart primary linux-swap -2GiB 100%
+parted /dev/sda -- mkpart ESP fat32 1MiB 512MiB
+parted /dev/sda -- set 3 esp on
+echo "partitioned successfully"
+
+echo "formatting..."
+mkfs.ext4 -L nixos /dev/sda1
+mkswap -L swap /dev/sda2
+mkfs.fat -F 32 -n boot /dev/sda3
+echo "formatted successfully"
+
+echo "mounting..."
+mount /dev/disk/by-label/nixos /mnt
+mkdir -p /mnt/boot
+mount /dev/disk/by-label/boot /mnt/boot
+swapon /dev/sda2
+echo "mounted successfully"
